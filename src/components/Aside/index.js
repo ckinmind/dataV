ddb48cb77a5556a42caaa2347647c59e7f20c36e
ckinmind <245160421@qq.com>
2017-04-06 10:46:10 +0800

    test

--- src/components/Aside/index.js
+++ src/components/Aside/index.js
@@ -9,9 +9,7 @@ class Aside extends React.Component {
               <nav>
                   <Link to="/lesson1"  activeClassName="active">Lesson 1</Link>
                   <Link to="/lesson2"  activeClassName="active">Lesson 2</Link>
-                  <Link to="/lesson3"  activeClassName="active">Lesson 3</Link>
-                  <Link to="/lesson4"  activeClassName="active">Lesson 4</Link>
-                  <Link to="/lesson5"  activeClassName="active">Lesson 5</Link>
+                 
 
                   <Link to="/item1"  activeClassName="active">Item1</Link>
                   <Link to="/item2"  activeClassName="active">Item2</Link>
